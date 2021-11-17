45c47d0f86a99c60ef396aa32eba95aa7b844697
vy-shoe <vadimulam6@gatech.edu>
2021-11-16 21:05:30 -0500

    expand snake when eating food

--- snake.js
+++ snake.js
@@ -1,9 +1,12 @@
 import { getInputDirection } from "./input.js";
 
-export const SNAKE_SPEED = 3;
+
+export const SNAKE_SPEED = 5;
 const snakeBody = [{ x: 11, y: 11 }];
+let newSegments = 0;
 
 export function update() {
+    addSegments()
     const inputDirection = getInputDirection();
     for (let i = snakeBody.length - 2; i >= 0; i--) {
         snakeBody[i + 1] = { ...snakeBody[i] };
@@ -21,4 +24,27 @@ export function draw(gameBoard) {
         snakeElement.classList.add("boba");
         gameBoard.appendChild(snakeElement);
     });
+}
+
+export function expandSnake(amount) {
+    newSegments += amount
+}
+
+// returns true if any snake body equals the snack, return true
+export function onSnake(position) {
+    return snakeBody.some(segment => {
+        return equalPositions(segment, position)
+    })
+}
+
+// return if snake head is same as food location 
+function equalPositions(pos1, pos2) {
+    return (pos1.x == pos2.x && pos1.y == pos2.y)
+}
+
+function addSegments() {
+    for (let i = 0; i < newSegments; i++) {
+        snakeBody.push({ ...snakeBody[snakeBody.length - 1] })
+    }
+    newSegments = 0
 }
