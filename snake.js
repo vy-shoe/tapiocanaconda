3976a2bd4adf814b0863e543d1e9ab77624adf7c
vy-shoe <vadimulam6@gatech.edu>
2021-12-01 23:36:08 -0500

    added a contrasting head boba and made background more 'appetizing' to match boba shop tiles

--- snake.js
+++ snake.js
@@ -1,59 +1,64 @@
-import { getInputDirection } from "./input.js"
+import { getInputDirection } from "./input.js";
 
-export const SNAKE_SPEED = 5
-const snakeBody = [{ x: 11, y: 11 }]
-let newSegments = 0
+export const SNAKE_SPEED = 5;
+const snakeBody = [{ x: 11, y: 11 }];
+let newSegments = 0;
 
 export function update() {
-    addSegments()
-    const inputDirection = getInputDirection()
+    addSegments();
+    const inputDirection = getInputDirection();
     for (let i = snakeBody.length - 2; i >= 0; i--) {
-        snakeBody[i + 1] = { ...snakeBody[i] }
+        snakeBody[i + 1] = { ...snakeBody[i] };
     }
 
-    snakeBody[0].x += inputDirection.x
-    snakeBody[0].y += inputDirection.y
+    snakeBody[0].x += inputDirection.x;
+    snakeBody[0].y += inputDirection.y;
 }
 
 export function draw(gameBoard) {
     snakeBody.forEach((segment) => {
-        const snakeElement = document.createElement("img")
-        snakeElement.setAttribute("src", "assets/boba" + (Math.floor(Math.random() * 3) + 1) + ".png");
-        snakeElement.style.gridRowStart = segment.y
-        snakeElement.style.gridColumnStart = segment.x
-        snakeElement.classList.add("boba")
-        gameBoard.appendChild(snakeElement)
+        const snakeElement = document.createElement("img");
+        if (segment == snakeBody[0]) {
+            snakeElement.setAttribute("src", "assets/boba" + 4 + ".png");
+        } else {
+            snakeElement.setAttribute("src", "assets/boba" + (Math.floor(Math.random() * 3) + 1) + ".png"
+            );
+        }
+        snakeElement.style.gridRowStart = segment.y;
+        snakeElement.style.gridColumnStart = segment.x;
+        snakeElement.classList.add("boba");
+        gameBoard.appendChild(snakeElement);
     });
 }
 
 export function expandSnake(amount) {
-    newSegments += amount
+    newSegments += amount;
 }
 
 // returns true if any snake body equals the snack, return true
-export function onSnake(position, { ignoreHead = false } = {} ) {
+export function onSnake(position, { ignoreHead = false } = {}) {
     return snakeBody.some((segment, index) => {
-        if (ignoreHead && index === 0) return false
-        return equalPositions(segment, position)
+        if (ignoreHead && index === 0) return false;
+        return equalPositions(segment, position);
     });
 }
 
 // return if snake head is same as food location
 function equalPositions(pos1, pos2) {
-    return pos1.x == pos2.x && pos1.y == pos2.y
+    return pos1.x == pos2.x && pos1.y == pos2.y;
 }
 
 function addSegments() {
     for (let i = 0; i < newSegments; i++) {
-        snakeBody.push({ ...snakeBody[snakeBody.length - 1] })
+        snakeBody.push({ ...snakeBody[snakeBody.length - 1] });
     }
-    newSegments = 0
+    newSegments = 0;
 }
 
 export function getSnakeHead() {
-    return snakeBody[0]
+    return snakeBody[0];
 }
 
 export function ifSnakeIntersects() {
-    return onSnake(snakeBody[0], {ignoreHead: true})
+    return onSnake(snakeBody[0], { ignoreHead: true });
 }
